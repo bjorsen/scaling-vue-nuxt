eaeafb047a9494d60f3bad17ab07d28e3596f374
Bjorn Van Hoeymissen <bjorn.van.hoeymissen@duo.be>
2020-07-11 19:18:39 +0200

    Refactored Axios API calls to async await

--- pages/index.vue
+++ pages/index.vue
@@ -17,20 +17,18 @@ import EventCard from '@/components/EventCard.vue'
 
 export default {
   components: { EventCard },
-  asyncData(context) {
-    return context.$axios
-      .get('http://localhost:3003/events')
-      .then((response) => {
-        return {
-          events: response.data,
-        }
+  async asyncData(context) {
+    try {
+      const response = await context.$axios.get('http://localhost:3003/events')
+      return {
+        events: response.data,
+      }
+    } catch (e) {
+      context.error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please try again.',
       })
-      .catch((e) => {
-        context.error({
-          statusCode: 503,
-          message: 'Unable to fetch events at this time. Please try again.',
-        })
-      })
+    }
   },
   head() {
     return {
